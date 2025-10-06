7735ff44081ee64f5218e2304233feebf6e0f826
Edwardroc <mdlzr13.gmail.com>
2025-10-06 10:06:00 -0500

    rutas en el main

--- ejercicios/src/main.ts
+++ ejercicios/src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 import './bases/03-object-literal'
 import './bases/04-arrays'
 import './bases/05-functions'
-
+import './bases/06-obj-destruturing'
+import './bases/07-array-destructuring'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Hola mundo</h1>
