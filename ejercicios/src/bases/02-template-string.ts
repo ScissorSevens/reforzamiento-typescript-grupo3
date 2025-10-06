0feee7753a7792313fe404a877308d4c171a2367
lauracast000009 <lauracatagomez46@gmail.com>
2025-10-06 07:09:32 -0500

    Agregando ejercicios 1 y 2

---
+++ ejercicios/src/bases/02-template-string.ts
@@ -0,0 +1,14 @@
+
+
+const nombre: string = "Laura";
+const carrera: string = "Ingeniería de Sistemas";
+const semestre: number = 5;
+
+const mensaje: string = `Hola, mi nombre es ${nombre}, estudio ${carrera} y estoy en el semestre ${semestre}.`;
+
+console.log(mensaje);
+
+/*
+Las template strings permiten insertar variables directamente dentro del texto
+  usando ${variable}. También facilitan escribir mensajes largos o con saltos de línea.
+*/
